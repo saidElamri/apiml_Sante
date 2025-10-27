1240a904e08f97ef28df910fcf2124ee658f40c3
elbahia-stack <boudalelbahia@gmail.com>
2025-10-27 14:58:54 +0100

    mise ajour

---
+++ schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field, conint, confloat, constr
+
+
+class PatientCreate(BaseModel):
+    age: conint(ge=0, le=120) = Field(..., description="Âge du patient entre 0 et 120")
+
+    gender: constr(min_length=1, max_length=10) = Field(..., description="Genre du patient")
+
+    status: constr(min_length=1, max_length=50) = Field(..., description="Statut du patient")
+
+    pressurhigh: confloat(ge=0) = Field(..., description="Tension artérielle systolique")
+    pressurlow:confloat(ge=0) = Field(..., description="Tension artérielle systolique")
+    glucose: confloat(ge=0) = Field(..., description="Taux de glucose")
+
+    kcm: confloat(ge=0) = Field(..., description="Valeur KCM")
+
+    troponin: confloat(ge=0) = Field(..., description="Taux de tropin")
+
+    implusem: conint(ge=0) = Field(..., description="Fréquence cardiaque / impulsion")
+
